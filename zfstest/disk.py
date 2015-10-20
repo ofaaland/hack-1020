3ea57fa9270a1e24005acdf33958f182fdafcdda
Allen Petersen <allen.petersen@osnexus.com>
2015-10-20 16:10:27 -0700

    Adding Linux disk code.

--- zfstest/disk.py
+++ zfstest/disk.py
@@ -3,5 +3,8 @@ import platform
 platform_name = platform.system()
 if platform_name == 'SunOS':
     from zfstest.disk_illumos import *
+elif platform_name == 'Linux':
+    from zfstest.disk_linux import *
+
 def foo():
     print 'hi'
---
+++ zfstest/disk_linux.py
@@ -0,0 +1,5 @@
+def find_free_disks():
+    return ['/dev/sdd1', '/dev/sdd2']
+
+def partition_disk(disk_name, num_slices):
+    pass
