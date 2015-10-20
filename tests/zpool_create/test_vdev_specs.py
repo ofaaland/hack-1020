92ea8ab1b88504358b6ca6418b0b8e26bf72d885
John Wren Kennedy <john.kennedy@delphix.com>
2015-10-20 23:04:59 +0000

    destroy

--- tests/zpool_create/test_vdev_specs.py
+++ tests/zpool_create/test_vdev_specs.py
@@ -1,17 +1,15 @@
 from zfstest import disk
 from zfstest import zpool
 
-def _create_pool_tests(pool_name, vdev_spec):
-    zpool.create(pool_name, vdev_spec)
-    return False
+def _create_pool_tests(pool_name, pool_type, vdev_spec):
+    # vdev_spec = vdev_spec[:2]
+    zpool.create(pool_name, pool_type, vdev_spec)
+    zpool.destroy(pool_name)
 
 
-def test_001():
-    vdev_specs = ['', 'mirror', 'raidz', 'raidz1', 'raidz2']
+def test_whole_disks():
+    pool_types = ['', 'mirror', 'raidz', 'raidz1', 'raidz2']
     disk_list = disk.find_free_disks()
 
-    #zpool.create('testpool', '', ['c1t1d0', 'c1t2d0'])
-    zpool.create('testpool', 'mirror', ['c1t1d0', 'c1t2d0'])
-
-    #for pool_type in pool_types:
-    #    _create_pool_tests('testpool', pool_type)
+    for pool_type in pool_types:
+        _create_pool_tests('testpool', pool_type, disk_list)
--- zfstest/zpool.py
+++ zfstest/zpool.py
@@ -13,8 +13,22 @@ def create(pool_name, pool_type, vdev_list):
     :param vdev_list: The vdevs needed to create the pool
     :type vdev_list: ``list`` of strings
     """
-    cmd = ['sudo', 'zpool', 'create', pool_name, pool_type]
+    cmd = ['sudo', 'zpool', 'create', pool_name]
+    if len(pool_type):
+        cmd += [pool_type]
     cmd += vdev_list
+
+    try:
+        proc = Popen(cmd, stdout=PIPE, stderr=PIPE, stdin=PIPE)
+        proc.stdin.close()
+        while proc.returncode is None:
+            proc.poll()
+    except:
+        pass
+
+
+def destroy(pool_name):
+    cmd = ['sudo', 'zpool', 'destroy', pool_name]
 
     try:
         proc = Popen(cmd, stdout=PIPE, stderr=PIPE, stdin=PIPE)
